50cf35a3742d8b5d15dbb76bbdfed9c03d6aa23c
MarcosBayas95 <marcos.bayase@ug.edu.ec>
2024-04-15 21:52:43 -0500

    Cambio login 2.0

--- proyectoinspi/src/componentes/Login/Login.js
+++ proyectoinspi/src/componentes/Login/Login.js
@@ -16,24 +16,29 @@ export const Login = () => {
   return (
     <body>
     <div id="contenedor">
-            <div id="central">
-                <div id="login">
-                    <div class="titulo">
-                    <img src="https://www.gob.ec/sites/default/files/styles/medium/public/2018-11/INSPI%20LOGO_0.png?itok=3n2KJyo2" alt="Logo de INSPI" />
-                    </div>
-                    <form id="loginform">
-                        <input onChange={(event)=>{setUsuario(event.target.value)}} type="text" name="usuario" placeholder="Usuario" required />
-                        
-                        <input onChange={(event)=>{setPassword(event.target.value)}} type="password" placeholder="Contraseña" name="password" required />
-                        
-                        <button type="submit" title="Ingresar" name="Ingresar" onClick={AccerLogin}>Acceder</button>
-                    </form>
-                    <div class="pie-form">
-                        <a href="#">¿No puedes Iniciar Sesión?</a>
-                    </div>
+    <div id="central">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />  
+        <div id="login">
+            <div class="titulo">
+                <img src="https://www.gob.ec/sites/default/files/styles/medium/public/2018-11/INSPI%20LOGO_0.png?itok=3n2KJyo2" alt="Logo de INSPI" />
+            </div>
+            <form id="loginform">
+                <div class="input-icon">
+                    <input onChange={(event) => { setUsuario(event.target.value); }} type="text" name="usuario" placeholder="Usuario" required />
+                    <i class="fa fa-user"></i>
                 </div>
+                <div class="input-icon">
+                    <input onChange={(event) => { setPassword(event.target.value); }} type="password" placeholder="Contraseña" name="password" required />
+                    <i class="fa fa-lock"></i>
+                </div>
+                <button type="submit" title="Ingresar" name="Ingresar" onClick={AccerLogin}>Acceder</button>
+            </form>
+            <div class="pie-form">
+                <a href="#">¿No puedes Iniciar Sesión?</a>
             </div>
         </div>
+    </div>
+</div>
     </body>
   );
 };
